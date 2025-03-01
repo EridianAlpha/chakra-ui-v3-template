74d1ebd1feff5b417ced3043f14ba76f441d9fe8
Eridian <86061486+EridianAlpha@users.noreply.github.com>
2025-03-01 11:10:13 +0000

    Update head metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
-"use client"
-
 import { Provider } from "./provider"
 import { ThemeProvider } from "next-themes"
 import type { ReactNode } from "react"
 
+import Head from "./head"
+
 export default function RootLayout({ children }: { children: ReactNode }) {
     return (
         <html lang="en" suppressHydrationWarning>
+            <Head />
             <body suppressHydrationWarning>
                 <ThemeProvider attribute="class">
                     <Provider>{children}</Provider>
